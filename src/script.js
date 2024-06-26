bacb01d2fbcb1b6fd4cb86c2a818c6ebe82b60e2
eilinseth <aksun989@gmail.com>
2024-06-26 21:14:58 +0700

    update portfolio

--- src/script.js
+++ src/script.js
@@ -9,9 +9,15 @@ menu.addEventListener('click',function(e){
 })
 
 document.addEventListener('click',function(e){
+    e.preventDefault();
     if(e.target !== nav && e.target !== menu){
         menu.classList.remove('active')
         nav.classList.remove('active')
+        
     }
     console.log(e.target)
+})
+
+window.addEventListener("mouseover",function(e){
+    console.log(e)
 })
