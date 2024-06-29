f78c0a90311cd812e31cb2fb588b24c872f11304
eilinseth <aksun989@gmail.com>
2024-06-30 05:50:41 +0700

    perbaikan kode

--- src/script.js
+++ src/script.js
@@ -1,36 +1,31 @@
-const menu = document.querySelector('.tombol');
-const nav = document.querySelector('#nav')
+const menu = document.querySelector(".tombol");
+const nav = document.querySelector("#nav");
 
-menu.addEventListener('click',function(e){
-    menu.classList.toggle('active')
-    nav.classList.toggle('active')
+menu.addEventListener("click", function (e) {
+  menu.classList.toggle("active");
+  nav.classList.toggle("active");
+});
 
-    
-})
+document.addEventListener("click", function (e) {
+  if (e.target !== nav && e.target !== menu) {
+    menu.classList.remove("active");
+    nav.classList.remove("active");
+  }
+  console.log(e.target);
+});
 
-document.addEventListener('click',function(e){
-    if(e.target !== nav && e.target !== menu){
-        menu.classList.remove('active')
-        nav.classList.remove('active')
-        
-    }
-    console.log(e.target)
-})
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+  const toTOp = document.querySelector("#toTop");
 
-window.onscroll = function(){
-    const header = document.querySelector('header');
-    const fixedNav = header.offsetTop;
-    const toTOp = document.querySelector('#toTop');
-
-    if(window.scrollY > fixedNav){
-        header.classList.add('navbar-fixed')
-        toTOp.classList.remove('hidden');
-        toTOp.classList.add('flex')
-    }else{
-        header.classList.remove('navbar-fixed');
-        toTOp.classList.remove('flex');
-        toTOp.classList.add('hidden');
-    }
-}
-
-
+  if (window.scrollY > fixedNav) {
+    header.classList.add("navbar-fixed");
+    toTOp.classList.remove("hidden");
+    toTOp.classList.add("flex");
+  } else {
+    header.classList.remove("navbar-fixed");
+    toTOp.classList.remove("flex");
+    toTOp.classList.add("hidden");
+  }
+};
